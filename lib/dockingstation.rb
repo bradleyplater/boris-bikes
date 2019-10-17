4599bb9e5a10c14534d1f927edfd0f229830ae82
bradley <bradley.plater1@gmail.com>
2019-10-17 10:47:12 +0100

    Added files for 2 new features

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -1,46 +1,47 @@
-require_relative 'Bike'
- 
+require_relative "Bike"
+
 class DockingStation
-    attr_reader :dock, :max_dock, :DEFAULT_CAPACITY
-    DEFAULT_CAPACITY = 20
+  attr_reader :dock, :max_dock, :DEFAULT_CAPACITY
+  DEFAULT_CAPACITY = 20
 
-    def initialize(capacity = DEFAULT_CAPACITY)
-        @max_dock = capacity
-        @dock = []
-    end
-    def release_bike
-        if !empty?
-            @dock.each_with_index do |bike, index|
-                if bike.working?
-                    @dock.delete_at(index)
-                    return bike
-                else
-                    raise "Bike is broken"
-                end
-            end
-         else
-            raise "No bikes to release"
-        end
-    end
+  def initialize(capacity = DEFAULT_CAPACITY)
+    @max_dock = capacity
+    @dock = []
+  end
 
-    def dock_bike (bike,working = true)
-        if !full?
-            @dock << bike
+  def release_bike
+    if !empty?
+      @dock.each_with_index do |bike, index|
+        if bike.working?
+          @dock.delete_at(index)
+          return bike
         else
-            raise "Too many bikes"
+          raise "Bike is broken"
         end
+      end
+    else
+      raise "No bikes to release"
     end
-    def show_dock
-        @dock.each do |el|
-            return el
-        end
-    end
+  end
 
-    private def full?
-        @dock.length == @max_dock
+  def dock_bike(bike, working = true)
+    if !full?
+      @dock << bike
+    else
+      raise "Too many bikes"
     end
-    private def empty?
-        @dock.length == 0
-    end
+  end
 
+  def show_dock
+    @dock.each do |el|
+      return el
+    end
+  end
+
+  private def full?
+    @dock.length == @max_dock
+  end
+  private def empty?
+    @dock.length == 0
+  end
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,54 +1,48 @@
-require 'DockingStation'
-require 'Bike'
+require "DockingStation"
+require "Bike"
 
-describe DockingStation do 
+describe DockingStation do
+  subject { DockingStation.new }
+  let(:bike) { double :bike, working?: false }
 
-    subject { DockingStation.new }
+  it "Responds to user releasing bike" do
+    expect(subject).to respond_to(:release_bike)
+  end
 
-    it 'Responds to user releasing bike' do
-        expect(subject).to respond_to(:release_bike)
-    end
+  it "Docks bike that user has" do
+    subject.dock_bike(bike)
+    expect(subject.dock).to include bike
+  end
 
-    it "Docks bike that user has" do 
-        bike = Bike.new
-        subject.dock_bike(bike)
-        expect(subject.dock).to include bike
-    end
+  it "sees the bike that has been docked" do
+    subject.dock_bike(bike)
+    expect(subject.show_dock).to eq bike
+  end
 
-    it "sees the bike that has been docked" do
-        bike = Bike.new
-        subject.dock_bike(bike)
-        expect(subject.show_dock).to eq bike
-    end
-        
-    it "Shows error if no bikes available" do
-        expect { subject.release_bike }.to raise_error "No bikes to release"
-    end
+  it "Shows error if no bikes available" do
+    expect { subject.release_bike }.to raise_error "No bikes to release"
+  end
 
-    it "shows error if max number of bikes are in dock" do
-        expect { 21.times{subject.dock_bike(Bike.new)} }.to raise_error "Too many bikes"
-    end 
+  it "shows error if max number of bikes are in dock" do
+    expect { 21.times { subject.dock_bike(double(:bike)) } }.to raise_error "Too many bikes"
+  end
 
-    it "Capacity is equal to the capacity set by user" do
-        dock = DockingStation.new(12)
-        expect(dock.max_dock).to eq 12
-    end
+  it "Capacity is equal to the capacity set by user" do
+    dock = DockingStation.new(12)
+    expect(dock.max_dock).to eq 12
+  end
 
-    it "Capacity is equal to default when no capacity set by user" do
-        expect(subject.max_dock).to eq DockingStation::DEFAULT_CAPACITY
-    end
+  it "Capacity is equal to default when no capacity set by user" do
+    expect(subject.max_dock).to eq DockingStation::DEFAULT_CAPACITY
+  end
 
-    it "Docking station does not release broken bike" do
-        bike = Bike.new
-        bike.report_broken
-        subject.dock_bike(bike)
-        expect { subject.release_bike }.to raise_error "Bike is broken"
-    end
+  it "Docking station does not release broken bike" do
+    subject.dock_bike(bike)
+    expect { subject.release_bike }.to raise_error "Bike is broken"
+  end
 
-    it "Docking station can store bike that is broken" do 
-        bike = Bike.new
-        bike.report_broken
-        subject.dock_bike(bike)
-        expect(subject.dock[0].working?).to eq false
-    end
+  it "Docking station can store bike that is broken" do
+    subject.dock_bike(bike)
+    expect(subject.dock[0].working?).to eq false
+  end
 end
